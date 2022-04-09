1f63cb2200277d9771b2d77cab61b28dfe9c7ed9
Daryl DeNobrega <dcdenobrega@gmail.com>
2022-04-09 09:45:07 -0400

    Updated Styles added header and footer background updated particle logic, updated colorscheme, updated contact form

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -42,6 +42,32 @@ export default class Footer extends Component {
 	render() {
 		return (
 			<footer>
+				<svg
+					className="hex"
+					xmlnsXlink="http://www.w3.org/1999/xlink"
+					width="100%"
+					height="100%"
+				>
+					<defs>
+						<pattern
+							id="hexagons"
+							width="50"
+							height="43.4"
+							patternUnits="userSpaceOnUse"
+							patternTransform="scale(5) translate(2) rotate(45)"
+						>
+							<polygon
+								points="24.8,22 37.3,29.2 37.3,43.7 24.8,50.9 12.3,43.7 12.3,29.2"
+								id="hex"
+							/>
+							<use xlinkHref="#hex" x="25" />
+							<use xlinkHref="#hex" x="-25" />
+							<use xlinkHref="#hex" x="12.5" y="-21.7" />
+							<use xlinkHref="#hex" x="-12.5" y="-21.7" />
+						</pattern>
+					</defs>
+					<rect width="100%" height="100%" fill="url(#hexagons)" />
+				</svg>
 				<Socials />
 				<div>
 					<h3>More Information</h3>
