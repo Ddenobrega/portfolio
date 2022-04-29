09a9cc12c92c0fc8c4333292a11a3293a0d0c20b
Daryl DeNobrega <dcdenobrega@gmail.com>
2022-04-28 22:47:27 -0400

    Resolved Typo

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,90 +5,86 @@ import { Icon } from "@iconify/react";
 import CV from "../Assets/Documents/CV.pdf";
 
 class Socials extends Component {
-	render() {
-		return (
-			<div>
-				<h3>Socials</h3>
-				<div>
-					<Icon className="icon" icon="bi:github" />
-					<a href="https://github.com/ddenobreaga">Github</a>
-				</div>
-				<div>
-					<Icon className="icon" icon="bi:linkedin" />
-					<a href="https://linkedin.com/in/dcdenobrega">Linkedin</a>
-				</div>
-				<div>
-					<Icon className="icon" icon="simple-icons:hackthebox" />
-					<a href="https://app.hackthebox.com/profile/975406">
-						Hack the box
-					</a>
-				</div>
-				<div>
-					<Icon className="icon" icon="akar-icons:facebook-fill" />
-					<a href="https://www.facebook.com/DarylDeNobrega/">
-						Facebook
-					</a>
-				</div>
-				<div>
-					<Icon className="icon" icon="akar-icons:twitter-fill" />
-					<a href="https://twitter.com/DcDenobrega">Twitter</a>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h3>Socials</h3>
+        <div>
+          <Icon className="icon" icon="bi:github" />
+          <a href="https://github.com/ddenobrega">Github</a>
+        </div>
+        <div>
+          <Icon className="icon" icon="bi:linkedin" />
+          <a href="https://linkedin.com/in/dcdenobrega">Linkedin</a>
+        </div>
+        <div>
+          <Icon className="icon" icon="simple-icons:hackthebox" />
+          <a href="https://app.hackthebox.com/profile/975406">Hack the box</a>
+        </div>
+        <div>
+          <Icon className="icon" icon="akar-icons:facebook-fill" />
+          <a href="https://www.facebook.com/DarylDeNobrega/">Facebook</a>
+        </div>
+        <div>
+          <Icon className="icon" icon="akar-icons:twitter-fill" />
+          <a href="https://twitter.com/DcDenobrega">Twitter</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default class Footer extends Component {
-	render() {
-		return (
-			<footer>
-				<svg
-					className="hex"
-					xmlnsXlink="http://www.w3.org/1999/xlink"
-					width="100%"
-					height="100%"
-				>
-					<defs>
-						<pattern
-							id="hexagons"
-							width="50"
-							height="43.4"
-							patternUnits="userSpaceOnUse"
-							patternTransform="scale(5) translate(2) rotate(45)"
-						>
-							<polygon
-								points="24.8,22 37.3,29.2 37.3,43.7 24.8,50.9 12.3,43.7 12.3,29.2"
-								id="hex"
-							/>
-							<use xlinkHref="#hex" x="25" />
-							<use xlinkHref="#hex" x="-25" />
-							<use xlinkHref="#hex" x="12.5" y="-21.7" />
-							<use xlinkHref="#hex" x="-12.5" y="-21.7" />
-						</pattern>
-					</defs>
-					<rect width="100%" height="100%" fill="url(#hexagons)" />
-				</svg>
-				<Socials />
-				<div>
-					<h3>More Information</h3>
-					<div>
-						<Icon icon="carbon:document" className="icon" />
-						<a href={CV} target="_blank" rel="noreferrer">
-							Resume
-						</a>
-					</div>
-					<div>
-						<Icon icon="bi:code-slash" className="icon" />
-						<a
-							href="https://github.com/ddenobrega/portfolio"
-							target="_blank"
-							rel="noreferrer"
-						>
-							Source Code
-						</a>
-					</div>
-				</div>
-			</footer>
-		);
-	}
+  render() {
+    return (
+      <footer>
+        <svg
+          className="hex"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          width="100%"
+          height="100%"
+        >
+          <defs>
+            <pattern
+              id="hexagons"
+              width="50"
+              height="43.4"
+              patternUnits="userSpaceOnUse"
+              patternTransform="scale(5) translate(2) rotate(45)"
+            >
+              <polygon
+                points="24.8,22 37.3,29.2 37.3,43.7 24.8,50.9 12.3,43.7 12.3,29.2"
+                id="hex"
+              />
+              <use xlinkHref="#hex" x="25" />
+              <use xlinkHref="#hex" x="-25" />
+              <use xlinkHref="#hex" x="12.5" y="-21.7" />
+              <use xlinkHref="#hex" x="-12.5" y="-21.7" />
+            </pattern>
+          </defs>
+          <rect width="100%" height="100%" fill="url(#hexagons)" />
+        </svg>
+        <Socials />
+        <div>
+          <h3>More Information</h3>
+          <div>
+            <Icon icon="carbon:document" className="icon" />
+            <a href={CV} target="_blank" rel="noreferrer">
+              Resume
+            </a>
+          </div>
+          <div>
+            <Icon icon="bi:code-slash" className="icon" />
+            <a
+              href="https://github.com/ddenobrega/portfolio"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Source Code
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
